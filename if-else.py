7bbc5d956d4436a1d81cadbcce89dce4be896292
zonnix <zontee5871@gmail.com>
2023-07-13 17:08:23 +0700

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,57 @@
+# if True:
+#     print("HELLO")
+
+# if False:
+#     print("hello")
+
+#     money = 100
+# if money >= 300:
+#     print("KFC!!!")
+# else:
+#     print("NO KFC")
+# Username = input("Enter Username:")
+# Password = input("Enter Password:")
+
+
+# if Username == "zontee" and Password == "42167":
+#        print("login succesful")
+# elif Username == "admin" and Password == "1234"
+#        print("login succesful1")
+# elif Username == "admin":
+#        print("only User")
+# else:
+#        print("false")
+
+# if Username == "zontee" and Password == "42167":
+#         print("login succesful")
+#         print("1 = Vatcal")
+#         print("2 = Cal")
+#         if num == "1":
+#             price = int(input("Enter your price"))
+#             vat = 7/100
+#             print(price * 1.07)
+#         elif num == "2":
+#             x = int(input("enter your number"))
+#             y = int(input("enter your number"))
+#             print(x+y)
+# else:
+#      print("false")            
+
+x = 0
+while x == 0: 
+    User = input("User:")
+    Pass = input("Pass:")
+    if User == "admin" and Pass == "1234":
+        print("done")
+        x = 1
+    else:
+        print("wrong")  
+
+# if User == "admin" and Pass == "1234":
+#     print("done")
+# else:
+#     print("false")
+    
+
+        
+                
