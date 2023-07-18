25099ffdcfe6e6515616381231148536a84c0fb6
zonnix <zontee5871@gmail.com>
2023-07-18 19:04:01 +0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,19 +1,4 @@
-score = int(input("Score:"))
-
-if score >= 80:
-    grade = "A"
-elif score >= 75:
-    grade = "B+"
-elif score >= 70:
-    grade = "B"
-elif score >= 65:
-    grade = "C+"
-elif score >= 60:
-    grade = "C"
-elif score >= 55:
-    grade = "D+"
-elif score >= 50:
-    grade = "D"
-else:
-    grade = "F"
-print("Grade:",grade)
+inputRound = int(input("Please Enter Number : "))
+sum = 0
+for x in range(int(inputRound)):
+    inputNumber = int(input("x"+str(x)+":"))
